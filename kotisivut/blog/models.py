6fcf4c8411a974fb5ea036697d1740605bfa6d2b
Janne Hyvönen <Janne.j.hyvonen@gmail.com>
2016-06-13 16:55:24 +0300

    tietokantataulu Post luotu ja lisätty blog-admin:iin. Pari testiblogausta luotu. Seuraavaksi blog view:n ja templaten päivitys ja postausten esittämisen testaaminen.

--- kotisivut/blog/models.py
+++ kotisivut/blog/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	title = models.CharField(max_length=200)
+	content = models.TextField()
+
+	def __unicode__(self):
+		return self.title

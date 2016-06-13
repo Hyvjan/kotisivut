9e5efdf4a98a100d44887fcdcef4891c7b799897
Janne Hyvönen <Janne.j.hyvonen@gmail.com>
2016-06-13 23:23:17 +0300

    Blog view:ssä lisätty postausten välitys templatelle ja template päivitetty näyttämään postaukset. Testattu manuaalisesti ja toimii.

--- kotisivut/blog/views.py
+++ kotisivut/blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Context, loader
+from blog.models import Post
 
 # Create your views here.
 def posts_list(request):
+	all_posts = Post.objects.all().order_by('-created_at')
 	t=loader.get_template('blog/posts.html')
-	return HttpResponse(t.render())
-	
+	c = Context({'all_posts': all_posts, })
+	return HttpResponse(t.render(c))

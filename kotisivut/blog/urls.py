80065b51b584b758a8904d7eb2d9ba07c25491df
Janne Hyvönen <Janne.j.hyvonen@gmail.com>
2016-06-16 14:54:15 +0300

    Lisätty view, template ja routing blogipostauksen lisäämiseksi. Testattu ja toimii. Seuraavaksi lisättävä autentikointi ja auktorisointi, että vain ylläpitäjä voi lisätä postauksia.

--- kotisivut/blog/urls.py
+++ kotisivut/blog/urls.py
@@ -4,5 +4,6 @@ from blog import views
 
 urlpatterns = [
 	url(r'^$', views.posts_list, name='posts_list'),
+	url(r'add_post/', views.add_post, name='add_post'),
 
 ]
--- kotisivut/blog/views.py
+++ kotisivut/blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, render_to_response, redirect
 from django.http import HttpResponse
-from django.template import Context, loader
+from django.template import Context, loader, RequestContext
 from blog.models import Post
+from blog.forms import PostForm
 
 # Create your views here.
 def posts_list(request):
@@ -9,3 +10,17 @@ def posts_list(request):
 	t=loader.get_template('blog/posts.html')
 	c = Context({'all_posts': all_posts, })
 	return HttpResponse(t.render(c))
+
+
+def add_post(request):
+	context=RequestContext(request)
+	if request.method == 'POST':
+		form = PostForm(request.POST, request.FILES)
+		if form.is_valid():
+			form.save(commit=True)
+			return redirect(index)
+		else:
+			print form.errors
+	else:
+		form=PostForm()
+	return render_to_response('blog/add_post.html', {'form':form},context)

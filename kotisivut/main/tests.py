affde45ea3abe34e574e823b7a0e46adcee389db
Janne Hyvönen <Janne.j.hyvonen@gmail.com>
2016-09-02 22:25:06 +0300

    sivujen ulkoasua hieman muutettu.

--- kotisivut/main/tests.py
+++ kotisivut/main/tests.py
@@ -7,6 +7,13 @@ from main.views import index
 
 # Create your tests here.
 class indexTests(TestCase):
-	def test_root_resolves_to_main_view(self):
+
+	def test_root_resolves_to_index_view(self):
 		landing_page=resolve('/')
 		self.assertEquals(landing_page.func, index)
+
+	def test_returns_correct_html(self):
+		katselukerrat_aluksi=viewCounter.objects.get(counterName='index')
+		index=self.client.get('/')
+		katselukerrat_lopuksi=viewCounter.objects.get(counterName='index')
+		self.assertEquals(katselukerrat_aluksi+1, katselukerrat_lopuksi)

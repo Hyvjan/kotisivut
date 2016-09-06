5df950657fbdffb67c364e7f656d51752a514735
Janne Hyvönen <Janne.j.hyvonen@gmail.com>
2016-09-06 20:47:45 +0300

    Lisätty testi jolla varmistetaan, että blog appsin Post model toimii ja että index-näkymän all_posts-haku toimii.

--- kotisivut/blog/tests.py
+++ kotisivut/blog/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, RequestFactory
 from main.models import viewCounter
+from blog.models import Post
 from django.core.urlresolvers import resolve, reverse
 from blog.views import posts_list, add_post
 from django.contrib.auth.models import User, AnonymousUser
@@ -24,6 +25,22 @@ class indexTests(TestCase):
 		katselukerrat_lopuksi=viewCounter.objects.get(counterName='blog').views
 		self.assertEquals(katselukerrat_aluksi+1, katselukerrat_lopuksi)
 
+
+	def test_index_query_for_all_posts_works(self):
+
+		post1=Post(title="post1", content="test posting 1")
+		post1.save()
+
+		post2=Post(title="post2", content="test posting 2")
+		post2.save()
+
+		all_posts = Post.objects.all().order_by('-created_at')
+
+		self.assertEquals(len(all_posts), 2)
+		self.assertEquals(all_posts[0].title, u'post2')
+		self.assertEquals(all_posts[1].title, u'post1')
+
+
 class add_postTests(TestCase):
 
 	def test_unlogged_user_cannot_access_add_post_view(self):
@@ -47,9 +64,30 @@ class add_postTests(TestCase):
 		response = add_post(request)
 
 		self.assertEqual(response.status_code, 200)
-		
-		
 
 
-		
+	"""	
+class delete_postTests(TestCase):
 
+	def test_unlogged_user_cannot_access_delete_post_view(self):
+		
+		self.factory = RequestFactory()
+		request = self.factory.get('/blog/delete_post/')
+		request.user = AnonymousUser()
+		response = add_post(request)
+
+		self.assertEqual(response.status_code, 302)
+
+	def test_logged_in_user_can_access_delete_post(self):
+
+		
+		self.factory = RequestFactory()
+		self.user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+		
+		request = self.factory.get('/blog/add_post/')
+		request.user = self.user
+		response = add_post(request)
+
+		self.assertEqual(response.status_code, 200)		
+
+"""
